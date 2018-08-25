770891c42810ee6af2b71ef02071f4342f732764
Alex Burns <30638935+alexmburns@users.noreply.github.com>
2018-08-25 23:07:21 +0100

    Delete console.py

--- console.py
+++
@@ -1,5 +0,0 @@
-#prints the contents of test.txt into console, remember to change directory to be valid.
-#need to adapt script for scroll pHAT HD
-
-print ''.join(file('/Users/Alex/Downloads/test.txt'))
-
